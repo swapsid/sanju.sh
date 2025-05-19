a70eda4fdcf964bdfa1e20ab7b744626548592f4
Swapnil Siddharth <39430010+swapsid@users.noreply.github.com>
2025-05-19 21:17:22 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.sanju.sh",
+	site: "https://www.swapsid.com",
 	integrations: [
 		mdx(),
 		sitemap({
