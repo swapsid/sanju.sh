04c93a212afd38022125a8834547dbdcaa7c71c3
Swapnil Siddharth <39430010+swapsid@users.noreply.github.com>
2025-05-19 21:54:03 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://www.swapsid.com",
-  integrations: [
-    mdx(),
-    sitemap({
-      changefreq: "daily",
-      priority: 1,
-      lastmod: new Date().toISOString().split("T")[0],
-    }),
-    tailwind(),
-  ],
-  output: "static",
+	site: "https://www.swapsid.com",
+	integrations: [
+		mdx(),
+		sitemap({
+			changefreq: "daily",
+			priority: 1,
+			lastmod: new Date().toISOString().split("T")[0],
+		}),
+		tailwind(),
+	],
+	output: "static",
+	// No adapter needed for static output
 });
